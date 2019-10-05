c728c8943d7fe9a1e5800581aead293d1f86415c
kil1menaz <kil1menaz@users.noreply.github.com>
2019-10-05 17:57:58 +0300

    class Product

---
+++ ForLearningGit/ForLearningGit/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLearningGit
+{
+    class Product
+    {
+        private Guid id;
+        private string name;
+        private double price;
+        private DateTime expirationDate = DateTime.Today;
+
+        public Product(Guid id, string name, double price, DateTime expirationDate)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.expirationDate = expirationDate;
+        }
+    }
+}
